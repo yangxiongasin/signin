e4953607a97dd75e2491e6cbc2e55e295a884082
hecz <1265806565@qq.com>
2018-12-04 17:08:11 +0800

    1

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ function setTime() {
     function systemTime() {
         var slidint;
         clearTimeout(slidint);
-        
+
         var dateTime = new Date();
         var hh = dateTime.getHours();
         var mm = dateTime.getMinutes();
@@ -44,7 +44,7 @@ function setTime() {
 
         $('.time-container .time').text(hh + ":" + mm + ":" + ss);
         slidint = setTimeout(systemTime, 1000);
-        
+
     }
 
     function extra(x) {
@@ -57,143 +57,251 @@ function setTime() {
 
 setTime();
 
-var clickFlag = true;
-var $leftContainer = $('.wrap-left .discern-container');
-var $rightContainer = $('.wrap-right .discern-container');
-var template = {
-    success: 
-    `
-    <div class="discern-list success">
-        <div class="discern-picture">
-            <img src="./image/successPic.png" />
-        </div>
-        <div class="discern-desc">
-            <p>欢迎您!</p>
-            <p>黄雅莉</p>
-        </div>
-    </div>
-    `,
-    fail:
-    `
-    <div class="discern-list fail">
-        <div class="discern-picture">
-            <img src="./image/failPic.png" />
-        </div>
-        <div class="discern-desc">
-            <p>欢迎贵宾!</p>
-        </div>
-    </div>
-    `
-};
-
-var i = 0;
-
-$('.wrap-left .title').click(function() {
-    // 保证最多只有3个识别项
-    if($leftContainer.find('.discern-list').length == 3) {
-        $leftContainer.find('.discern-list:first-child').remove();
-    }
-    // 载入识别HTML模板
-    var $dom = $(clickFlag? template.success : template.fail);
-    $leftContainer.append($dom);
-
-    if (clickFlag) {
-        // 保证最多只有8个识别项
-        if($rightContainer.find('.discern-list').length == 8) {
-            $rightContainer.find('.discern-list:last-child').remove();
-        }
-
-        $rightContainer.prepend(
-            `
-            <div class="discern-list">
-                <div class="discern-picture">
-                    <img src="./image/successPic.png" />
-                </div>
-                <div class="discern-desc">
-                    <p>姓名${i}</p>
-                </div>
-            </div>
-            `
-        )
-
-        i++;
-    }
-
-    // 20秒后自动销毁识别项
-    setTimeout(() => {
-        $dom.fadeOut(()=> {
-            $dom.remove();
-        });
-    }, 20000);
-
-    clickFlag = !clickFlag;
-});
-
-function resetLayout() {
-    var length = $leftContainer.find('.discern-list').length;
-}
-
-
 (function () {
-  /*判断是否可以循环*/
-  var hasStarted = false;
-  var interval = false;
-  var currentIndex = 0;
-  var width = 580;
-  var length = $(".slider-left-item").length;
-  /*给默认的图片*/
+    /*判断是否可以循环*/
+    var hasStarted = false;
+    var interval = false;
+    var currentIndex = 0;
+    var width = 580;
+    var length = $(".slider-left-item").length;
+    /*给默认的图片*/
 
-  $(".slider-left-item:not(:first)").removeClass("slider-item-selected");
-  $(".slider-left-item:first").addClass("slider-item-selected");
-  $(".slider-left-page").hide();
-  start();
-
-  /*面面item标签事件*/
-  $(".slider-left-item").hover(function (e) {
-    stop();
-    var currentIndex = $(this).index();
-    $(".slider-left-item").removeClass("slider-item-selected");
-    $(".slider-left-item").eq(currentIndex).addClass("slider-item-selected");
-    $(".slider-left-page").show();
-    goto(currentIndex);
-
-  },function (e) {
+    $(".slider-left-item:not(:first)").removeClass("slider-item-selected");
+    $(".slider-left-item:first").addClass("slider-item-selected");
     $(".slider-left-page").hide();
     start();
-  });
 
-  /*banner图片停止轮播*/
-  $(".slider-left-panel, .slider-left-page").hover(function (e) {
-    $(".slider-left-page").show();
-    clearInterval(interval);
-    stop();
-  },function (e) {
-    $(".slider-left-page").hide();
-    start();
-  });
-  /*开始运行*/
-  function start() {
-    if(!hasStarted){
-      hasStarted = true;
-      interval = setInterval(function () {
-        currentIndex = (++currentIndex+length)%length;
-        var offset = width*currentIndex;
+    /*面面item标签事件*/
+    $(".slider-left-item").hover(function (e) {
+        stop();
+        var currentIndex = $(this).index();
         $(".slider-left-item").removeClass("slider-item-selected");
         $(".slider-left-item").eq(currentIndex).addClass("slider-item-selected");
+        $(".slider-left-page").show();
+        goto(currentIndex);
 
-        $(".slider-left-main").animate({marginLeft:"-"+offset+"px"});
-      }, 10000);
+    }, function (e) {
+        $(".slider-left-page").hide();
+        start();
+    });
+
+    /*banner图片停止轮播*/
+    $(".slider-left-panel, .slider-left-page").hover(function (e) {
+        $(".slider-left-page").show();
+        clearInterval(interval);
+        stop();
+    }, function (e) {
+        $(".slider-left-page").hide();
+        start();
+    });
+    /*开始运行*/
+    function start() {
+        if (!hasStarted) {
+            hasStarted = true;
+            interval = setInterval(function () {
+                currentIndex = (++currentIndex + length) % length;
+                var offset = width * currentIndex;
+                $(".slider-left-item").removeClass("slider-item-selected");
+                $(".slider-left-item").eq(currentIndex).addClass("slider-item-selected");
+
+                $(".slider-left-main").animate({ marginLeft: "-" + offset + "px" });
+            }, 10000);
+        }
     }
-  }
-  /*停止轮播*/
-  function stop() {
-    clearInterval(interval);
-    hasStarted = false;
-  }
-  function goto(current) {
-    var offset = current*width;
-    $(".slider-left-item").removeClass("slider-item-selected");
-    $(".slider-left-item").eq(current).addClass("slider-item-selected");
-    $(".slider-left-main").animate({marginLeft:"-"+offset+"px"});
-  }
-})()
+    /*停止轮播*/
+    function stop() {
+        clearInterval(interval);
+        hasStarted = false;
+    }
+    function goto(current) {
+        var offset = current * width;
+        $(".slider-left-item").removeClass("slider-item-selected");
+        $(".slider-left-item").eq(current).addClass("slider-item-selected");
+        $(".slider-left-main").animate({ marginLeft: "-" + offset + "px" });
+    }
+})();
+
+var clickFlag = true;
+var $leftContainer = $('.wrap-left .discern-content');
+var $rightContainer = $('.wrap-right .discern-container');
+
+function template(data, flag) {
+    var template = {
+        success:
+            `
+        <div class="discern-list success">
+            <div class="discern-picture">
+                <img src="${data.image}" />
+            </div>
+            <div class="discern-desc">
+                <p>欢迎您!</p>
+                <p>${data.name}</p>
+            </div>
+        </div>
+        `,
+        fail:
+            `
+        <div class="discern-list fail">
+            <div class="discern-picture">
+                <img src="./image/failPic.png" />
+            </div>
+            <div class="discern-desc">
+                <p>欢迎贵宾!</p>
+            </div>
+        </div>
+        `
+    };
+
+    return flag ? template.success : template.fail
+}
+
+function checkName(name) {
+    return name != '100001' && name != '100002';
+}
+
+var timeout,
+    translateX = 219;
+
+try {
+
+    var ws = new WebSocket("ws://10.21.38.67:58000/chat");
+
+    ws.onmessage = function (e) {
+
+        var data = JSON.parse(e.data);
+
+        clearTimeout(timeout);
+
+        // 识别结果
+        var checkFlag = checkName(data.name);
+
+        // 载入HTML模板
+        var $dom = template(data, checkFlag);
+
+        switch ($leftContainer.find('.discern-list').length) {
+            case 0:
+                translateX = 219;
+                break;
+
+            case 1:
+                translateX = 109;
+                break;
+
+            case 2:
+                translateX = 0;
+                break;
+
+            default:
+                translateX -= 219;
+                break;
+        }
+
+        $leftContainer.css('transform', `translateX(${translateX}px)`);
+        
+        $leftContainer.append($dom);
+
+        if (checkFlag) {
+            // 保证最多只有8个识别项
+            if ($rightContainer.find('.discern-list').length == 8) {
+                $rightContainer.find('.discern-list:last-child').remove();
+            }
+
+            $rightContainer.prepend(
+                `
+                <div class="discern-list">
+                    <div class="discern-picture">
+                        <img src="${data.image}" />
+                    </div>
+                    <div class="discern-desc">
+                        <p>${data.name}</p>
+                    </div>
+                </div>
+                `
+            )
+        }
+
+
+        // 20秒后自动销毁识别项
+        timeout = setTimeout(() => {
+            $leftContainer.empty();
+            $leftContainer.css('transform', 'translateX(219px)');
+        }, 20000);
+    };
+
+} catch (ex) {
+
+    alert(ex.message);
+
+}
+
+// var clickFlag = true;
+
+// $('.header').click(function () {
+//     var DATA = [{
+//         name: '贺成璋',
+//         image: "./image/successPic.png"
+//     },{
+//         name: '杨雄',
+//         image: "./image/failPic.png"
+//     }]
+
+//     data = DATA[clickFlag? 0 : 1];
+
+//     clearTimeout(timeout);
+
+//     // 识别结果
+//     var checkFlag = checkName(data.name);
+
+//     // 载入HTML模板
+//     var $dom = template(data, checkFlag);
+
+//     switch ($leftContainer.find('.discern-list').length) {
+//         case 0:
+//             translateX = 219;
+//             break;
+
+//         case 1:
+//             translateX = 109;
+//             break;
+
+//         case 2:
+//             translateX = 0;
+//             break;
+
+//         default:
+//             translateX -= 219;
+//             break;
+//     }
+
+//     $leftContainer.css('transform', `translateX(${translateX}px)`);
+    
+//     $leftContainer.append($dom);
+
+//     if (checkFlag) {
+//         // 保证最多只有8个识别项
+//         if ($rightContainer.find('.discern-list').length == 8) {
+//             $rightContainer.find('.discern-list:last-child').remove();
+//         }
+
+//         $rightContainer.prepend(
+//             `
+//             <div class="discern-list">
+//                 <div class="discern-picture">
+//                     <img src="${data.image}" />
+//                 </div>
+//                 <div class="discern-desc">
+//                     <p>${data.name}</p>
+//                 </div>
+//             </div>
+//             `
+//         )
+//     }
+
+//     clickFlag = ! clickFlag;
+
+//     // 20秒后自动销毁识别项
+//     timeout = setTimeout(() => {
+//         $leftContainer.empty();
+//         $leftContainer.css('transform', 'translateX(219px)');
+//     }, 10000);
+// });
